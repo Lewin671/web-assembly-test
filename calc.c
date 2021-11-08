21742a3cd13a68d679336247e4711aa93a2a1744
Lewin671 <2596736318@qq.com>
2021-11-09 00:52:31 +0800

    chore: update test range

--- calc.c
+++ calc.c
@@ -4,7 +4,7 @@
 long long EMSCRIPTEN_KEEPALIVE calc()
 {
     long long result = 1;
-    for (int i = 1; i <= 1000000; i++)
+    for (int i = 1; i <= 10000000; i++)
     {
         result += i;
     }
